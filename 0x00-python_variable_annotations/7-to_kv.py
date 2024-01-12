61011f8a38abc3c8d3612f9c42915e4155c8a9ec
Nawaritta <Nor.benhaddou@gmail.com>
2024-01-12 17:46:40 +0000

    change Union order

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ returns a tuple(k, v*v)"""
     return (k, v ** 2)
