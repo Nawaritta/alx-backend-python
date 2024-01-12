8df04c38e55ef4cce730c80944a6d4e5c48592be
Nawaritta <Nor.benhaddou@gmail.com>
2024-01-12 23:26:40 +0000

    task 10 && 11

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+""" This module includes a Duck typing annontation"""
+
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ The function returns the first element of lst if the sequence is not
+empty or None if it is empty."""
+    if lst:
+        return lst[0]
+    else:
+        return None
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+""" This module includes a more involved type annontation"""
+
+from typing import Any, Mapping, TypeVar, Union, Optional
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Optional[T] = None) -> Union[Any, T]:
+    """ The function returns a key in a dictionnary if it
+    exists or default if not"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
