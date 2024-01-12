9bc9d9673b31812c98746f216bfc47d361dc9369
Nawaritta <Nor.benhaddou@gmail.com>
2024-01-12 00:24:03 +0000

    task 7 import Tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ This module defines a type-annotated function to_kv """
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[float, int]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
     """returns a tuple.
     The first element of the tuple is the string k.
     The second element is the square of the int/float v
