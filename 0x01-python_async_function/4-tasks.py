fe86a8f99170c761c30c8295e0c2910df93ad470
Nawaritta <Nor.benhaddou@gmail.com>
2024-01-15 20:49:28 +0000

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""This module defines an asynchronous coroutine task_wait_n"""
+
+import asyncio
+from typing import List
+module = __import__("3-tasks")
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """return the list of all the delays in ascending order"""
+    delays = []
+    for i in range(n):
+        delays.append(module.task_wait_random(max_delay))
+    return [await delay for delay in asyncio.as_completed(delays)]
