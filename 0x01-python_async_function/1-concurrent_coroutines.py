be413d14d378c53d8fa7ae432645dd58af5bb03c
Nawaritta <Nor.benhaddou@gmail.com>
2024-01-15 20:06:05 +0000

    task1 use asyncio.as_completed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,12 +10,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """return the list of all the delays in ascending order"""
     delays = []
     for i in range(n):
-        el = await module.wait_random(max_delay)
-        j = 0
-        while (j < len(delays)):
-            if delays[j] < el:
-                j += 1
-            else:
-                break
-        delays.insert(j, el)
-    return delays
+        delays.append(module.wait_random(max_delay))
+    return [await delay for delay in asyncio.as_completed(delays)]
