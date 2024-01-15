46d8eb47a67004f870c50fa664a9e4070c148268
Nawaritta <Nor.benhaddou@gmail.com>
2024-01-15 20:36:22 +0000

    measure_time not an asynchronous coroutine

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """This module defines an asynchronous coroutine measure_time"""
 
-import asyncio
 import time
+import asyncio
 module = __import__("1-concurrent_coroutines")
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """measures the total execution time for wait_n(n, max_delay)"""
     start = time.time()
-    await module.wait_n(n, max_delay)
+    asyncio.run(module.wait_n(n, max_delay))
     end = time.time()
     return (end - start) / n
