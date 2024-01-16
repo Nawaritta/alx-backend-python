503c24f183657e45701d29d0288175e703902ce9
Nawaritta <Nor.benhaddou@gmail.com>
2024-01-16 15:46:24 +0000

    tasks 1 && 2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""This module contains async_generators coroutine"""
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """collects 10 random numbers using an async comprehensing
+    over async_generator, then return the 10 random numbers."""
+    return [result async for result in async_generator()]
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""This module contains measure_runtime coroutine"""
+import asyncio
+import time
+async_compr = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Execute async_comprehension four times in parallel using asyncio.gather.
+    and measure the total runtime and return it."""
+    start = time.time()
+    coroutines = [async_compr() for _ in range(4)]
+    await asyncio.gather(*coroutines)
+    runtime = time.time() - start
+    return runtime
